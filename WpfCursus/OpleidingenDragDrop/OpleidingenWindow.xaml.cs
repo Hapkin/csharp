c055b696ae2881d7f291fc4a49a384620d2cdf25
Unknown <jo.vansant@vdabcampus.be>
2018-07-03 11:51:50 +0200

    ribbon

--- WpfCursus/OpleidingenDragDrop/OpleidingenWindow.xaml.cs
+++ WpfCursus/OpleidingenDragDrop/OpleidingenWindow.xaml.cs
@@ -33,5 +33,86 @@ namespace WPFCursus
             ListBoxProgrammas.Items.Add(new OfficeProgramma("Word", new BitmapImage(new Uri(@"images\word.png", UriKind.Relative))));
             ListBoxProgrammas.SelectedIndex = 0;
         }
+
+        private ListBoxItem VindListBoxItem(Object sleepitem)
+        {
+            DependencyObject keuze = (DependencyObject)sleepitem;
+            while (keuze != null)
+            {
+                if (keuze is ListBoxItem)
+                    return (ListBoxItem)keuze;
+                keuze = VisualTreeHelper.GetParent(keuze);
+            }
+            return null;
+        }
+
+
+
+        /// DRAG EVENT
+
+        ListBox draglijst;
+        private void DragListBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+            }
+            draglijst = (ListBox)sender;
+            ListBoxItem programmaitem = VindListBoxItem(e.OriginalSource);
+            if (draglijst.SelectedIndex >= 0 && programmaitem != null)
+            {
+                DataObject sleepdata =
+                new DataObject("mijnprogramma", programmaitem.Content);
+                DragDrop.DoDragDrop(programmaitem, sleepdata,
+                DragDropEffects.Move);
+            }
+        }
+
+        /// DROP EVENT
+        private void DropListBox_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent("mijnprogramma"))
+            {
+                OfficeProgramma sleepprogramma = (OfficeProgramma)e.Data.GetData("mijnprogramma");
+                ListBox droplijst = (ListBox)sender;
+                if (draglijst != droplijst)
+                {
+                    droplijst.Items.Add(sleepprogramma);
+                    draglijst.Items.Remove(draglijst.SelectedItem);
+                }
+            }
+        }
+        /// BUTTON
+        private void ButtonDoorgeven_Click(object sender, RoutedEventArgs e)
+        {
+            String tekst = "Volgende programma's zijn:\n";
+            if (ListBoxProgrammas.Items.Count > 0)
+            {
+                tekst += "\nNiet toegewezen: ";
+                foreach (OfficeProgramma prog in ListBoxProgrammas.Items)
+                {
+                    tekst += prog.naam + ", ";
+                }
+                tekst = tekst.Substring(0, tekst.Length - 2);
+            }
+            if (ListBoxGekend.Items.Count > 0)
+            {
+                tekst += "\nGekend: ";
+                foreach (OfficeProgramma prog in ListBoxGekend.Items)
+                {
+                    tekst += prog.naam + ", ";
+                }
+                tekst = tekst.Substring(0, tekst.Length - 2);
+            }
+            if (ListBoxTeVolgen.Items.Count > 0)
+            {
+                tekst += "\nTe volgen: ";
+                foreach (OfficeProgramma prog in ListBoxTeVolgen.Items)
+                {
+                    tekst += prog.naam + ", ";
+                }
+                tekst = tekst.Substring(0, tekst.Length - 2);
+            }
+            MessageBox.Show(tekst, "Overzicht");
+        }
     }
 }
