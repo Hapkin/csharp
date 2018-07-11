6215edea360affd19006d598ca52402cc80a2638
Unknown <jo.vansant@vdabcampus.be>
2018-07-11 19:54:56 +0200

    pizzanet

---
+++ WPFpizzanet/WPFpizzanet/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFpizzanet
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Model.Bestellingen mijnTekst = new Model.Bestellingen();
+            ViewModel.PizzanetVM vm = new ViewModel.PizzanetVM(mijnTekst);
+            View.PizzanetWindow mijnTekstboxView = new View.PizzanetWindow();
+            mijnTekstboxView.DataContext = vm;
+            mijnTekstboxView.Show();
+        }
+    }
+}
