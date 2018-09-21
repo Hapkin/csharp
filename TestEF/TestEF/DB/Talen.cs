edf83e67ba41abfb5c19c97bdc6f10e3d45f0b27
Jo Vansant <jo_vansant@hotmail.com>
2018-09-21 15:40:28 +0200

    all

--- TestEF/TestEF/DB/Talen.cs
+++ TestEF/TestEF/DB/Talen.cs
@@ -21,6 +21,7 @@ namespace TestEF.DB
 
         [Required]
         [StringLength(50)]
+
         public string Naam { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
